5003831c550fcd33e719f758b35d89d398cc90c4
Anderson Alves <andersonmsalves@gmail.com>
2025-07-18 12:02:42 -0300

    add logout functionality

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Link from "next/link";
+import { useAuth } from "@/resources";
+import { useRouter } from "next/navigation";
 
 interface TemplateProps {
   children: React.ReactNode;
@@ -52,10 +57,34 @@ export const RenderIf: React.FC<RenderIfProps> = ({
 };
 
 export const Header: React.FC = () => {
+  const auth = useAuth();
+  const user = auth.getUserSession();
+  const router = useRouter();
+
+  function logout() {
+    auth.invalidateSession();
+    router.push("/login");
+  }
+
   return (
     <header className="bg-indigo-950 text-white py-3">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <h1 className="text-3x1 font-bold">ImageLite</h1>
+        <Link href="/galeria">
+          <h1 className="text-3x1 font-bold">ImageLite</h1>
+        </Link>
+
+        <RenderIf condition={!!user?.name}>
+          <div className="flex items-center">
+            <div className="relative">
+              <span className="w-64 py-3 px-6 text-md">Olá, {user?.name}</span>
+              <span className="w-64 py-3 px-6 text-sm">
+                <a href="#" onClick={logout}>
+                  Sair
+                </a>
+              </span>
+            </div>
+          </div>
+        </RenderIf>
       </div>
     </header>
   );
